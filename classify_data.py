05aa6b8f4d67c228926e705f18f2792a7452a0b1
Breandan Considine <breandan.considine@gmail.com>
2017-11-12 22:10:06 -0500

    add cleaner CLI for classify_data.py

--- classify_data.py
+++ classify_data.py
@@ -5,16 +5,28 @@ import scipy.misc, scipy.ndimage
 import numpy as np
 import sys
 
-print("Loading model...")
-model = load_model(sys.argv[1])
-print("Loaded model!")
+if len(sys.argv) < 2:
+    model_path = 'data/temp_model.hdf5'
+else:
+    model_path = sys.argv[1]
+
+if len(sys.argv) < 3:
+    output_path = 'data/test_y.csv'
+else:
+    output_path = sys.argv[2]
+
+print("Loading model from " + model_path + "...")
+model = load_model(model_path)
 
 print("Loading test data...")
 x_test = load_testing_data()
+
 print("Removing background...")
 x_test = threshold(x_test)
+
 print("Removing dots...")
 x_test = remove_dots(x_test)
+
 x_test = x_test.reshape(-1, 64, 64, 1)
 
 print("Printing data...")
@@ -23,7 +35,8 @@ predictions = model.predict(x_test)
 labels, _, _ = get_labels()
 labels = list(labels)
 
-with open('data/test_y.csv', 'w+') as output:
+print("Writing predictions to " + output_path + "...")
+with open(output_path, 'w+') as output:
     print("Id,Label", file=output)
     for i, prediction in enumerate(predictions):
         print(str(i + 1) + "," + str(labels[np.argmax(prediction)]), file=output)
